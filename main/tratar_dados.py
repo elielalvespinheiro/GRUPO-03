54caab4f86d8c44d065b08cb0acc1f2db3af11f0
elielalvespinheiro <elielalvespinheiro3456@gmail.com>
2025-05-06 10:12:09 -0400

    Second Commit

--- main/tratar_dados.py
+++ main/tratar_dados.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv(r'C:\Users\aluno noturno\Desktop\grupo03\Arquivo-trabalho\Documento-Limpo-grupo03.csv', delimiter=',', encoding='UTF-8')
+
 # tambaqui, pacu, tambacu, tilápia e pirarucu sobrevivem em um intervalo relativamente grande de temperatura
 # (de 80C - 100C, até cerca de 400C - 450C).
 # Entretanto, a temperatura ótima para o crescimento dessas espécies situa-se entre 250C e 320C.
@@ -26,3 +28,9 @@ import matplotlib.pyplot as plt
 # A concentração da forma tóxica aumenta com a elevação do pH e da temperatura. 
 # Concentração acima de 0,01 mg/l passa a afetar o crescimento e a resistência a doenças. 
 # O único tratamento economicamente eficiente para reduzir níveis de amônia em viveiros é a renovação de água.
+
+# no geral, bons resultados na produção são
+# alcançados em temperaturas da água entre 26 e 30°C, podendo variar de espécie para espécie.
+
+# Meça a temperatura usando um termômetro ou aparelho
+# digital pela manhã (6 h) e à tarde (17 h)
